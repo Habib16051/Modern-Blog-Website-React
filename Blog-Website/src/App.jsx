1b299179cb77b2c69f5e3adcebeaebb21810983f
habib-technonext <habibur.rahman@technonext.com>
2024-06-05 15:56:44 +0600

    modified

---
+++ Blog-Website/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Welcome to my Blog</h1>
+    </>
+  );
+}
+
+export default App;
